7529a06e06ce11c9f8af07307bd9c1034070042c
Ujeverson <ujeverson@gmail.com>
2017-07-04 15:31:47 -0300

    Testando
    verificar alteração

--- Fatorial/src/br/com/puts/FatorialUm.java
+++ Fatorial/src/br/com/puts/FatorialUm.java
@@ -6,7 +6,7 @@ public class FatorialUm {
 		for (int n = 1; n<=10; n++){
 			fatorial = n*fatorial;
 			System.out.println("O fatorial de "+n+" é "+n+"! = "+fatorial);
-		}
+		}//verificar a alteração.
 	}
 
 }
