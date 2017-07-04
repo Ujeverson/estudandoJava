d46ee422cf7a93652d14ba64bfffe78988bfabf5
Ujeverson <ujeverson@gmail.com>
2017-07-04 15:48:09 -0300

    int

--- Fatorial/src/br/com/puts/MultiploDeTres.java
+++ Fatorial/src/br/com/puts/MultiploDeTres.java
@@ -4,8 +4,8 @@ public class MultiploDeTres {
 	public static void main(String[] args) {
 		// imprimir todos os múltiplos de 3, de 1 a 100.
 
-		int i;
-		for (i = 1; i <= 102; i++) {
+		
+		for (int i = 1; i <= 102; i++) {
 			if (i % 3 == 0) {
 				System.out.print(i);
 			}else if(i%3 == 1){
