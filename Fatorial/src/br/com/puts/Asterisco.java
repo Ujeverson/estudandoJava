e3857c51709cf700e9c8c2e2fc19257d83a1e6c2
Ujeverson <ujeverson@gmail.com>
2017-07-06 08:17:07 -0300

    asteriscos

--- Fatorial/src/br/com/puts/Asterisco.java
+++ Fatorial/src/br/com/puts/Asterisco.java
@@ -1,16 +1,18 @@
 package br.com.puts;
 
 public class Asterisco {
+	
+	
+	
 	public static void main(String[] args) {
 		int n = 4;
 
 		for (int i = 1; i <= n; i++) {
-
-			if (i == n) {
-				System.out.println("");
+			for (int j = 1; j <= n; j++) {
+				System.out.print("*");
 			}
-			
+			System.out.println();
 		}
-	}
 
+	}
 }
